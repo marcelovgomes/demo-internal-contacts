e0fdb60f3eb580880ae50fb618cbfc73cfbed22f
d074177 <d074177@c02ww0bzhtd8.dhcp.muc2.sap.corp>
2018-08-24 17:12:46 +0200

    .

--- src/main/java/demo/internal/contacts/domain/Person.java
+++ src/main/java/demo/internal/contacts/domain/Person.java
@@ -3,15 +3,22 @@ package demo.internal.contacts.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Person {
 
+	/*
 	@Id
 	@GeneratedValue
 	private Long id;
-
+	*/
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+	
 	@Column(name = "name", length = 128)
 	private String name;
 	
